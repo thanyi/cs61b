16c31e17eccbe1ea90df6f12ab7eff85eac97b2d
ethaiyi9 <yiceyuan@126.com>
2024-09-28 11:24:15 +0800

    lab8 hashmap done

--- lab8/hashmap/MyHashMap.java
+++ lab8/hashmap/MyHashMap.java
@@ -1,6 +1,6 @@
 package hashmap;
 
-import java.util.Collection;
+import java.util.*;
 
 /**
  *  A hash table-backed Map implementation. Provides amortized constant time
@@ -28,11 +28,21 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
     /* Instance Variables */
     private Collection<Node>[] buckets;
     // You should probably define some more!
+    // 存储所有的key值
+    private Set<K> keySet = new HashSet<>();
+    // 用于存储初始化桶数
+    private int initialSize = 16;
+    private double loadFactor = 0.75;
 
     /** Constructors */
-    public MyHashMap() { }
+    public MyHashMap() {
+        buckets = createTable(this.initialSize);
+    }
 
-    public MyHashMap(int initialSize) { }
+    public MyHashMap(int initialSize) {
+        this.initialSize = initialSize;
+        buckets = createTable(this.initialSize);
+    }
 
     /**
      * MyHashMap constructor that creates a backing array of initialSize.
@@ -41,13 +51,17 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
      * @param initialSize initial size of backing array
      * @param maxLoad maximum load factor
      */
-    public MyHashMap(int initialSize, double maxLoad) { }
+    public MyHashMap(int initialSize, double maxLoad) {
+        this.initialSize = initialSize;
+        this.loadFactor = maxLoad;
+        buckets = createTable(this.initialSize);
+    }
 
     /**
      * Returns a new node to be placed in a hash table bucket
      */
     private Node createNode(K key, V value) {
-        return null;
+        return new Node(key,value);
     }
 
     /**
@@ -69,7 +83,7 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
      * OWN BUCKET DATA STRUCTURES WITH THE NEW OPERATOR!
      */
     protected Collection<Node> createBucket() {
-        return null;
+        return new ArrayList<>();
     }
 
     /**
@@ -82,10 +96,151 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
      * @param tableSize the size of the table to create
      */
     private Collection<Node>[] createTable(int tableSize) {
-        return null;
+
+        return new Collection[tableSize];
     }
 
     // TODO: Implement the methods of the Map61B Interface below
     // Your code won't compile until you do so!
 
+
+    @Override
+    public boolean containsKey(K key) {
+        return this.keySet.contains(key);
+    }
+
+    @Override
+    public int size() {
+        return keySet.size();
+    }
+
+
+    /**
+     * 进行hash处理，计算将其分配进bucket的序列号
+     * 使用mod运算
+     * @return 第几个bucket
+     */
+    public int hash(K key) {
+        // buckets中的序列号
+        int hashNum = key.hashCode();
+        return Math.floorMod(hashNum, buckets.length);
+    }
+
+    public int hash(K key, Collection[] buckets) {
+        // buckets中的序列号
+        int hashNum = key.hashCode();
+        return Math.floorMod(hashNum, buckets.length);
+    }
+
+    @Override
+    public V get(K key) {
+        if (!containsKey(key))
+            return null;
+
+        int bucketIdx = hash(key);
+        if (buckets[bucketIdx] != null){
+            for (Node node : buckets[bucketIdx]) {
+                if (node.key.equals(key)) {
+                    return node.value;
+                }
+            }
+        }
+
+        return null;
+
+    }
+
+
+    @Override
+    public Set<K> keySet() {
+        return keySet;
+    }
+
+    @Override
+    public void put(K key, V value) {
+
+        Node nodeAdd = new Node(key,value);
+        int bucketIdx = hash(key);
+        /* 如果不存在这个bucket */
+        if (buckets[bucketIdx]== null){
+            buckets[bucketIdx] = createBucket();
+        }
+        /* 如果已经存在这个key */
+        if (containsKey(key)){
+            for (Node node : buckets[bucketIdx]) {
+                if (node.key == key){
+                    node.value = value;
+                }
+            }
+        }else {
+            // 如果不存在
+            buckets[bucketIdx].add(nodeAdd);
+        }
+        keySet.add(key);
+        /* 如果此时需要进行resize */
+        if( keySet.size() >= loadFactor * this.buckets.length){
+            resize(2 * this.buckets.length);
+        }
+
+
+    }
+
+    /**
+     * 如果此时 keySize / buckets.length > loadFactor
+     * 则需要重新进行buckets的大小设计
+     */
+    public void resize(int size){
+        Collection<Node>[] tmpTable = createTable(size);
+        Collection<Node>[] oldbuckets = this.buckets;
+
+        for (var bucket: oldbuckets) {
+            if (bucket != null){
+                for(var node: bucket){
+                    int tmpIdx = hash(node.key,tmpTable);
+                    // 如果tmpTable中这一项中的数据结构不存在
+                    if(tmpTable[tmpIdx] == null)
+                        tmpTable[tmpIdx] = createBucket();
+
+                    tmpTable[tmpIdx].add(node);
+                }
+            }
+
+        }
+
+        this.buckets = tmpTable;
+
+    }
+
+    @Override
+    public V remove(K key) {
+        return remove(key,null);
+    }
+
+    @Override
+    public V remove(K key, V value) {
+        int bucketIdx = hash(key);
+        if (!containsKey(key)){
+            return null;
+        }
+        V valueRemove = get(key);
+        buckets[bucketIdx].remove(key);
+
+        keySet.remove(key);
+        return valueRemove;
+    }
+
+    @Override
+    public void clear() {
+        for (var bucket :
+                buckets) {
+            bucket = null;
+        }
+        keySet.clear();
+    }
+
+
+    @Override
+    public Iterator<K> iterator() {
+        return keySet.iterator();
+    }
 }
